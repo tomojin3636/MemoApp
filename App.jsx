c3786284643e6f8ef98b448903b3220c59da7a83
tomojin3636 <tomojin_y123@outlook.jp>
2022-06-27 11:28:25 +0900

    Save memo on  firestore

--- App.jsx
+++ App.jsx
@@ -12,11 +12,13 @@ import SingUpScreen from './src/screens/SingUpScreen';
 
 import { firebaseConfig } from './env';
 
-const Stack = createStackNavigator();
+require('firebase/firestore');
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
